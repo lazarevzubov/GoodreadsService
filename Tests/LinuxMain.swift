ebabf45ca8a74bbf1ded0ef744b3e88a8eadd9f8
Lazarev-Zubov, Nikita <nikita.lazarev-zubov@f-secure.com>
2024-06-16 18:19:10 +0300

    Remove legacy files from test target

--- Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import GoodreadsServiceTests
-import XCTest
-
-var tests = [XCTestCaseEntry]()
-tests += BookInfoXMLParserTests.allTests
-tests += SearchBooksXMLParser.allTests
-tests += WebDefaultServiceTests.allTests
-
-XCTMain(tests)
