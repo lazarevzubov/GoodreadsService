2cace90a90d342398404ec2f61cdccb29120f0b6
Lazarev-Zubov, Nikita <nikita.lazarev-zubov@f-secure.com>
2024-06-16 18:06:45 +0300

    Add description to Book

--- Sources/GoodreadsService/Book.swift
+++ Sources/GoodreadsService/Book.swift
@@ -14,6 +14,8 @@ public struct Book: Sendable {
 
     /// Authors of the book.
     public let authors: [String]
+    /// A description of the book.
+    public let description: String
     /// Goodreads ID of the book.
     public let id: String
     /// The book cover.
@@ -32,12 +34,19 @@ public struct Book: Sendable {
     /// - Parameters:
     ///   - authors: Authors of the book.
     ///   - title: The book title.
+    ///   - description: A description of the book.
     ///   - id: The ID of the book.
     ///   - imageURL: The book cover.
     ///   - similarBookIDs: Similar books IDs.
-    public init(authors: [String], title: String, id: String, imageURL: URL? = nil, similarBookIDs: [String] = []) {
+    public init(authors: [String],
+                title: String,
+                description: String,
+                id: String,
+                imageURL: URL? = nil,
+                similarBookIDs: [String] = []) {
         self.authors = authors
         self.title = title
+        self.description = description
         self.id = id
         self.imageURL = imageURL
         self.similarBookIDs = similarBookIDs
